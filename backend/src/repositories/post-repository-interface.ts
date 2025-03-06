5ff0ee5d82c513542c7af89f305925586988702c
Yuri <yurivgr@icloud.com>
2025-03-06 09:17:06 -0300

    post repository remodel

--- backend/src/repositories/post-repository-interface.ts
+++ backend/src/repositories/post-repository-interface.ts
@@ -3,8 +3,9 @@ import { Post } from "@prisma/client";
 
 export interface PostRepository {
   create(authorId: string, data: PostSchema): Promise<Post>;
-  update(id: string, data: Partial<PostSchema>): Promise<Post>;
+  getById(id: string): Promise<Post>
   getByUser(authorId: string): Promise<Post[]>;
+  update(id: string, data: Partial<PostSchema>): Promise<Post>;
   delete(id: string): Promise<void>;
   getCommentsByPost(id: string): Promise<CommentSchema[]>;
 }
--- backend/src/repositories/post-repository.ts
+++ backend/src/repositories/post-repository.ts
@@ -33,12 +33,24 @@ export const postRepository: PostRepository = {
       throw new InternalServerError(`Error Finding Posts By: ${authorId}`);
     }
   },
+  async getById(id) {
+    try {
+      const post = await prisma.post.findUnique({ where: { id } });
+
+      if (!post) throw new NotFoundError("Post Not Found");
+
+      return post;
+    } catch (error) {
+      if (error instanceof NotFoundError) throw error;
+      throw new InternalServerError(`Error Finding Post: ${id}`);
+    }
+  },
   async delete(id) {
     try {
       await prisma.post.delete({ where: { id } });
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === "P2025") throw new NotFoundError("User not found.");
+        if (error.code === "P2025") throw new NotFoundError("Post Not Found.");
       }
 
       throw new InternalServerError("Error Deleting Post");
@@ -46,6 +58,10 @@ export const postRepository: PostRepository = {
   },
   async getCommentsByPost(id) {
     try {
+      const post = await prisma.post.findUnique({ where: { id } });
+
+      if (!post) throw new NotFoundError("Post Not Found");
+
       return await prisma.comment.findMany({ where: { postId: id } });
     } catch (error) {
       if (error instanceof Prisma.PrismaClientKnownRequestError) {
